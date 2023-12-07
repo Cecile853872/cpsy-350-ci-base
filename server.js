c4231235783c10d157e0488fe60faf28aad5ef89
cecile <cecile.yogore@edu.sait.ca>
2023-12-06 19:50:39 -0700

    test8

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:80000');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:80001');
 });
 
 const server = app.listen(PORT, HOST);
